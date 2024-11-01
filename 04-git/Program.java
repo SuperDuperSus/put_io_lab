90ec130a0622783be4fd4c4c0d9d75a32374761c
VladosLavandos <uladzislau.pastushok@student.put.poznan.pl>
2024-11-01 23:14:06 +0100

    add java

---
+++ 04-git/Program.java
@@ -0,0 +1,38 @@
+public class Program {
+    // Class variables
+    private String name;
+    private int version;
+
+    // Constructor
+    public Program(String name, int version) {
+        this.name = name;
+        this.version = version;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Getter for version
+    public int getVersion() {
+        return version;
+    }
+
+    // Setter for version
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        Program program = new Program("Example", 1);
+        System.out.println("Program Name: " + program.getName());
+        System.out.println("Program Version: " + program.getVersion());
+    }
+}
